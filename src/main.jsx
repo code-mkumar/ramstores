fd3c85855daa6911ebc20cdce495ade1ef9c0974
mukesh <22us25@anjaconline.org>
2025-12-02 15:27:52 +0530

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,8 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <GoogleOAuthProvider clientId="369885088133-j9n7d76p6aukqljpb76fpsfu68bq76mj.apps.googleusercontent.com">
-      <App />
-    </GoogleOAuthProvider>
-  </StrictMode>,
+  <GoogleOAuthProvider clientId="369885088133-j9n7d76p6aukqljpb76fpsfu68bq76mj.apps.googleusercontent.com">
+  <App />
+</GoogleOAuthProvider>
+,
 )
